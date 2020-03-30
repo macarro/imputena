2962a93f5f5e92ebd3e3ff66cd3878ae10638283
macarro <migmackle@alum.us.es>
2020-03-30 11:02:09 +0200

    Wrote docstring for LOCF

--- imputena/locf.py
+++ imputena/locf.py
@@ -2,6 +2,25 @@ import pandas as pd
 
 
 def locf(data=None, columns=None, fill_leading=False, inplace=False):
+    """Fills in NA values with the last observation in the same column. If
+    fill_leading is true, leading values are filled in with the first
+    observation. The operation can be applied to a series, a whole
+    dataframe, or a selection of columns of a dataframe.
+
+    :param data: The data on which to perform the LOCF operation
+    :type data: pandas.Series or pandas.DataFrame
+    :param columns: Columns on which to apply the operation.
+    :type columns: array-like, optional
+    :param fill_leading: Whether to fill in leading NA values with the first
+    observation.
+    :type fill_leading: bool, optional
+    :param inplace: If True, do operation inplace and return None.
+    :type inplace: bool, optional
+    :return: The series or dataframe with NA values filled in, or
+    None if inplace=True.
+    :rtype: pandas.Series, pandas.DataFrame, or None
+    :raises: TypeError, ValueError
+    """
     if not (isinstance(data, pd.Series) or isinstance(data, pd.DataFrame)):
         raise TypeError('The data has to be a Series or DataFrame.')
     if isinstance(data, pd.Series) and columns is not None:
@@ -47,4 +66,3 @@ def locf(data=None, columns=None, fill_leading=False, inplace=False):
                 if fill_leading:
                     res[column].fillna(method='bfill', inplace=True)
             return res
-
