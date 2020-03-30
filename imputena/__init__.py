ed1b9fbc5f3967138711f59e563010e3dde04651
macarro <migmackle@alum.us.es>
2020-03-30 10:05:11 +0200

    Import functions explicitly

--- imputena/__init__.py
+++ imputena/__init__.py
@@ -1,3 +1,4 @@
-from .delete_listwise import *
-from .delete_pairwise import *
-from .delete_columns import *
+from .delete_listwise import delete_listwise
+from .delete_pairwise import delete_pairwise
+from .delete_columns import delete_columns
+
