6bd70e60046bfe7931228bad1a9a17e1c5ff2572
macarro <migmackle@alum.us.es>
2020-03-31 13:34:42 +0200

    Simplified code

--- imputena/locf.py
+++ imputena/locf.py
@@ -27,42 +27,39 @@ def locf(data=None, columns=None, fill_leading=False, inplace=False):
         raise ValueError('Columns can only be selected if the data is a '
                          'DataFrame.')
     if columns is None:
-        # Treatment for a series or all columns of a dataframe:
+        # Treatment for a series or all columns of a dataframe
         if inplace:
+            # Apply locf:
             data.fillna(method='ffill', inplace=True)
+            # If fill_leading, apply nocb:
             if fill_leading:
                 data.fillna(method='bfill', inplace=True)
             return None
         else:
             if fill_leading:
+                # Apply locf, then nocb:
                 return data.fillna(method='ffill').fillna(method='bfill')
             else:
+                # Apply locf only:
                 return data.fillna(method='ffill')
     else:
+        # Treatment for selected columns of a dataframe
         if inplace:
-            for column in columns:
-                # Raise error if the column name doesn't exist in the data:
-                if column not in data.columns:
-                    raise ValueError(
-                        '\'' + column + '\' is not a column of the data.'
-                    )
-                # Apply locf:
-                data[column].fillna(method='ffill', inplace=True)
-                # If fill_leading, apply nocb:
-                if fill_leading:
-                    data[column].fillna(method='bfill', inplace=True)
-            return None
+            res = data
         else:
             res = data.copy()
-            for column in columns:
-                # Raise error if the column name doesn't exist in the data:
-                if column not in data.columns:
-                    raise ValueError(
-                        '\'' + column + '\' is not a column of the data.'
-                    )
-                # Apply locf:
-                res[column].fillna(method='ffill', inplace=True)
-                # If fill_leading, apply nocb:
-                if fill_leading:
-                    res[column].fillna(method='bfill', inplace=True)
+        for column in columns:
+            # Raise error if the column name doesn't exist in the data:
+            if column not in data.columns:
+                raise ValueError(
+                    '\'' + column + '\' is not a column of the data.'
+                )
+            # Apply locf:
+            res[column].fillna(method='ffill', inplace=True)
+            # If fill_leading, apply nocb:
+            if fill_leading:
+                res[column].fillna(method='bfill', inplace=True)
+        if inplace:
+            return None
+        else:
             return res
