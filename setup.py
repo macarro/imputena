698fd960260ef881302210ed25c1707b79c3b170
macarro <migmackle@alum.us.es>
2020-04-23 21:50:30 +0200

    Added PyPi classifiers

--- setup.py
+++ setup.py
@@ -16,9 +16,15 @@ setuptools.setup(
     author_email='migmackle@alum.us.es',
     license='MIT',
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Financial and Insurance Industry'
     ],
     python_requires='>=3.5',
     install_requires=[
