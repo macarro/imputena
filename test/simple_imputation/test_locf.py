fb3157e2ad746f424a08f40c95b86cc84f9ec5c3
macarro <migmackle@alum.us.es>
2020-05-18 13:07:30 +0200

    Added test for LOCF: column for series

--- test/simple_imputation/test_locf.py
+++ test/simple_imputation/test_locf.py
@@ -230,6 +230,22 @@ class TestLOCF(unittest.TestCase):
         with self.assertRaises(TypeError):
             locf(data)
 
+    def test_LOCF_col_for_series(self):
+        """
+        Negative test
+
+        data: Correct series (example_series)
+        columns: ['a'] (series can't have columns)
+
+        Checks that the function raises a ValueError if a column is passed
+        for a series.
+        """
+        # 1. Arrange
+        ser = generate_example_series()
+        # 2. Act & 3. Assert
+        with self.assertRaises(ValueError):
+            locf(ser, columns=['a'])
+
     def test_LOCF_df_returning_wrong_column(self):
         """
         Negative test
